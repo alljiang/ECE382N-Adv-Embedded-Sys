a27299c2a38cfd32b0d13f44fe673346ee2c238e
Allen Jiang <alljiang@hotmail.com>
2024-02-08 11:10:56 -0600

    fifo test

--- AXI4_BURST_MASTER/FIFO.v
+++ AXI4_BURST_MASTER/FIFO.v
@@ -31,8 +31,19 @@ module MY_FIFO #(parameter depth=8)
     output fifo_full,
     output fifo_empty
 );
+
+    reg [31:0] memory[7:0];
+
+    assign memory[0] = 8'hfffffff0;
+    assign memory[1] = 8'hfffffff1;
+    assign memory[2] = 8'hfffffff2;
+    assign memory[3] = 8'hfffffff3;
+    assign memory[4] = 8'hfffffff4;
+    assign memory[5] = 8'hfffffff5;
+    assign memory[6] = 8'hfffffff6;
+    assign memory[7] = 8'hfffffff7;
     
-    reg [31:0] memory[depth:0];
+    // reg [31:0] memory[depth:0];
     reg [$clog2(depth)-1:0] write_ptr;
     reg [$clog2(depth)-1:0] read_ptr;
     reg [$clog2(depth):0] count;
@@ -75,7 +86,7 @@ module MY_FIFO #(parameter depth=8)
         else begin
             if (write_en & !fifo_full) begin
                 write_ptr <= write_ptr + 1;
-                memory[write_ptr] <= write_data;
+                // memory[write_ptr] <= write_data;
             end
         end
     
