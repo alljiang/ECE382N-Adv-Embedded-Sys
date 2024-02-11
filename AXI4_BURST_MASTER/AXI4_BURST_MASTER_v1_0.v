1e177111c374b36246338aebb415e349d84ac2c6
Allen Jiang <alljiang@hotmail.com>
2024-02-11 15:24:48 -0600

    reduce timer precision

--- AXI4_BURST_MASTER/AXI4_BURST_MASTER_v1_0.v
+++ AXI4_BURST_MASTER/AXI4_BURST_MASTER_v1_0.v
@@ -137,8 +137,8 @@
         .reads_done(read_done),
         .writes_done(write_done),
         .txn_done(tester_done),
-        .timer_write(timer_write),
-        .timer_read(timer_read),
+        .timer_write(timer_write[39:8]),
+        .timer_read(timer_read[39:8]),
         .debug1(debug1),
         .debug2(debug2),
         .debug3(debug3),
@@ -216,8 +216,9 @@
     localparam [1:0] STATE_AWAIT_COMPARE = 2'b10;
     reg [1:0] tester_state;
 
-    reg [31:0] timer_write;
-    reg [31:0] timer_read;
+    // throw away last 8 bits of timer
+    reg [40:0] timer_write;
+    reg [40:0] timer_read;
     
     always @(posedge m00_axi_aclk) begin
         if (!m00_axi_aresetn || init_txn_pulse) begin
