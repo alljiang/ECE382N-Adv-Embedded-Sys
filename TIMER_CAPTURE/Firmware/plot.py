482b3bf589bc539cbf595ffd1e6059470cdaa5a2
Allen Jiang <alljiang@hotmail.com>
2024-03-06 17:08:16 -0600

    finish lab 2g

--- TIMER_CAPTURE/Firmware/plot.py
+++ TIMER_CAPTURE/Firmware/plot.py
@@ -1,5 +1,6 @@
 # parse and plot the data in data.txt
 
+import numpy as np
 import matplotlib.pyplot as plt
 
 file = open("data.txt", "r")
@@ -8,12 +9,22 @@ file.close()
 
 min_dat = []
 max_dat = []
+x = np.arange(0, len(lines), 1)
 
 for line in lines:
     split = line.split(' ')
-    min_dat.append(split[0])
-    max_dat.append(split[1])
+    min_dat.append(float(split[0]))
+    max_dat.append(float(split[1].split('\n')[0]))
+
+    print("{}".format(max_dat[-1]))
 
 # scatter plot
 plt.figure()
-
+plt.scatter(x, min_dat, label='min')
+plt.scatter(x, max_dat, label='max')
+plt.yscale('log')
+plt.xlabel('Sample Number')
+plt.ylabel('Latency (us)')
+plt.title('Latency Measurements')
+plt.legend()
+plt.show()
