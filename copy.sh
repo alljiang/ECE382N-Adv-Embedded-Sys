2c25a90c8057b0437875a191f6d699cce72feea3
Allen Jiang <alljiang@hotmail.com>
2024-03-06 17:15:07 -0600

    add infra for lab 3

--- copy.sh
+++ copy.sh
@@ -1,12 +1,19 @@
-cp AXI4_BURST_MASTER/AXI4_BURST_MASTER_v1_0_M00_AXI.v /misc/scratch/ajiang1/ip_repo/AXI4_BURST_MASTER_1_0/hdl/AXI4_BURST_MASTER_v1_0_M00_AXI.v
-cp AXI4_BURST_MASTER/AXI4_BURST_MASTER_v1_0_S00_AXI.v /misc/scratch/ajiang1/ip_repo/AXI4_BURST_MASTER_1_0/hdl/AXI4_BURST_MASTER_v1_0_S00_AXI.v
-cp AXI4_BURST_MASTER/AXI4_BURST_MASTER_v1_0.v /misc/scratch/ajiang1/ip_repo/AXI4_BURST_MASTER_1_0/hdl/AXI4_BURST_MASTER_v1_0.v
+# cp AXI4_BURST_MASTER/AXI4_BURST_MASTER_v1_0_M00_AXI.v /misc/scratch/ajiang1/ip_repo/AXI4_BURST_MASTER_1_0/hdl/AXI4_BURST_MASTER_v1_0_M00_AXI.v
+# cp AXI4_BURST_MASTER/AXI4_BURST_MASTER_v1_0_S00_AXI.v /misc/scratch/ajiang1/ip_repo/AXI4_BURST_MASTER_1_0/hdl/AXI4_BURST_MASTER_v1_0_S00_AXI.v
+# cp AXI4_BURST_MASTER/AXI4_BURST_MASTER_v1_0.v /misc/scratch/ajiang1/ip_repo/AXI4_BURST_MASTER_1_0/hdl/AXI4_BURST_MASTER_v1_0.v
 
-cp AXI4_BURST_MASTER/FIFO.v /misc/scratch/ajiang1/BASELINE_SP_2024/ultra96v2_oob.tmp/AXI4_BURST_MASTER_v1_0_project/AXI4_BURST_MASTER_v1_0_project.srcs/sources_1/new/FIFO.v
-cp AXI4_BURST_MASTER/FIFO_TB.v /misc/scratch/ajiang1/BASELINE_SP_2024/ultra96v2_oob.tmp/AXI4_BURST_MASTER_v1_0_project/AXI4_BURST_MASTER_v1_0_project.srcs/sim_1/new/FIFO_TB.v
+# cp AXI4_BURST_MASTER/FIFO.v /misc/scratch/ajiang1/BASELINE_SP_2024/ultra96v2_oob.tmp/AXI4_BURST_MASTER_v1_0_project/AXI4_BURST_MASTER_v1_0_project.srcs/sources_1/new/FIFO.v
+# cp AXI4_BURST_MASTER/FIFO_TB.v /misc/scratch/ajiang1/BASELINE_SP_2024/ultra96v2_oob.tmp/AXI4_BURST_MASTER_v1_0_project/AXI4_BURST_MASTER_v1_0_project.srcs/sim_1/new/FIFO_TB.v
 
-cp AXI4_BURST_MASTER/PATTERN_GEN.v /misc/scratch/ajiang1/BASELINE_SP_2024/ultra96v2_oob.tmp/AXI4_BURST_MASTER_v1_0_project/AXI4_BURST_MASTER_v1_0_project.srcs/sources_1/new/PATTERN_GEN.v
-cp AXI4_BURST_MASTER/PATTERN_GEN_TB.v /misc/scratch/ajiang1/BASELINE_SP_2024/ultra96v2_oob.tmp/AXI4_BURST_MASTER_v1_0_project/AXI4_BURST_MASTER_v1_0_project.srcs/sim_1/new/PATTERN_GEN_TB.v
+# cp AXI4_BURST_MASTER/PATTERN_GEN.v /misc/scratch/ajiang1/BASELINE_SP_2024/ultra96v2_oob.tmp/AXI4_BURST_MASTER_v1_0_project/AXI4_BURST_MASTER_v1_0_project.srcs/sources_1/new/PATTERN_GEN.v
+# cp AXI4_BURST_MASTER/PATTERN_GEN_TB.v /misc/scratch/ajiang1/BASELINE_SP_2024/ultra96v2_oob.tmp/AXI4_BURST_MASTER_v1_0_project/AXI4_BURST_MASTER_v1_0_project.srcs/sim_1/new/PATTERN_GEN_TB.v
 
-cp TIMER_CAPTURE/TIMER_CAPTURE_AXI_v1_0.v /misc/scratch/ajiang1/ip_repo/TIMER_CAPTURE_AXI_1_0/hdl/TIMER_CAPTURE_AXI_v1_0.v
-cp TIMER_CAPTURE/TIMER_CAPTURE_AXI_v1_0_S00_AXI_inst.v /misc/scratch/ajiang1/ip_repo/TIMER_CAPTURE_AXI_1_0/hdl/TIMER_CAPTURE_AXI_v1_0_S00_AXI.v
+# cp TIMER_CAPTURE/TIMER_CAPTURE_AXI_v1_0.v /misc/scratch/ajiang1/ip_repo/TIMER_CAPTURE_AXI_1_0/hdl/TIMER_CAPTURE_AXI_v1_0.v
+# cp TIMER_CAPTURE/TIMER_CAPTURE_AXI_v1_0_S00_AXI_inst.v /misc/scratch/ajiang1/ip_repo/TIMER_CAPTURE_AXI_1_0/hdl/TIMER_CAPTURE_AXI_v1_0_S00_AXI.v
+
+cp SHA3_BURST_MASTER/SHA3_BURST_MASTER_v1_0_M00_AXI.v /misc/scratch/ajiang1/Lab3/ip_repo/SHA3_BURST_MASTER_1_0/hdl/SHA3_BURST_MASTER_v1_0_M00_AXI.v 
+cp SHA3_BURST_MASTER/SHA3_BURST_MASTER_v1_0_S00_AXI.v /misc/scratch/ajiang1/Lab3/ip_repo/SHA3_BURST_MASTER_1_0/hdl/SHA3_BURST_MASTER_v1_0_S00_AXI.v 
+cp SHA3_BURST_MASTER/SHA3_BURST_MASTER_v1_0.v /misc/scratch/ajiang1/Lab3/ip_repo/SHA3_BURST_MASTER_1_0/hdl/SHA3_BURST_MASTER_v1_0.v 
+
+cp capture_timer/capture_timer_v1_0_S00_AXI.v /misc/scratch/ajiang1/Lab3/ip_repo/capture_timer_1.0/hdl/capture_timer_v1_0_S00_AXI.v
+cp capture_timer/capture_timer_v1_0.v /misc/scratch/ajiang1/Lab3/ip_repo/capture_timer_1.0/hdl/capture_timer_v1_0.v
